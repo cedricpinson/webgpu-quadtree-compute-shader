c3b85b43639e072733e01af5e91819e438c0f3fb
trigrou <trigrou@trigrou.com>
2023-07-01 13:41:33 +0200

    Fixes global path in build script

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ rm -rf dist docs/*.js docs/*.map docs/textures
 npx parcel build
 mkdir -p dist/textures && cp textures/elevation.png textures/earth.jpg dist/textures/
 cp -a dist/* docs/
+cat docs/index.html | sed 's/.//77' >docs/fixed.html
+mv docs/fixed.html docs/index.html
